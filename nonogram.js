437d4bcc21bcce7408459cdb99fae94ff36386dc
naourk <sofiamikaelafrancisco@gmail.com>
2025-09-06 15:58:31 +0400

    Create function to generate nonogram clues

--- nonogram.js
+++ nonogram.js
@@ -14,16 +14,41 @@ function splitColumns(ngm) {
         let newcol = [];
         for (let row = 0; row < ngm.length; row++) {
             newcol.push(ngm[row][col]);
-            // console.log(ngm[row][col]);
         }
         columns.push(newcol);
     }
 
-    columns.forEach(i => {
-        console.log(i);
-    });
-    
+    return columns;
 }
 
-// splitColumns(test5x5);
+function generateClues(ngm) {
+    let clues = []
+    
+    ngm.forEach(row => {
+        let newRowClues = []
+        let newClue = 0;
 
+        row.forEach(i => {
+            if (i == 1) newClue += 1;
+            else if (i == 0 && newClue == 0) return;
+            else if (i == 0 && newClue != 0) {
+                newRowClues.push(newClue);
+                newClue = 0;
+            } 
+            else console.log("Error")
+        })
+
+        if (newClue != 0) newRowClues.push(newClue);
+        if (newRowClues.length == 0) newRowClues.push(0);
+        clues.push(newRowClues);
+    });
+
+    return clues
+}
+
+
+const testclues = generateClues(splitColumns(test5x5));
+
+testclues.forEach(i => {
+    console.log(i)
+})
